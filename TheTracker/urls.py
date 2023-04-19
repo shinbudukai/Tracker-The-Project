ce20f5eb4221f5d2f72ba602277f42f15c331580
Gdeepika56 <dg1201@usnh.edu>
2023-04-19 15:10:33 -0400

    add todo_lists

--- TheTracker/urls.py
+++ TheTracker/urls.py
@@ -1,27 +1,43 @@
 from django.contrib import admin
 from django.urls import path
-
-from home.views import HomepageView
-
-"""TheTracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from django.urls import include
+from expenseTracker import views as etrack
+from todo_app import views as tViews
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomepageView.as_view(), name='home'),
+   # path("test/", tViews.test, name="test"),
+    path('', etrack.home, name='home'),
+    path('todo_app/', include('todo_app.urls')),
+    path('index/', etrack.index, name='index'),
+    path('register/',etrack.register,name='register'),
+    path('handleSignup/',etrack.handleSignup,name='handleSignup'),
+    path('handlelogin/',etrack.handlelogin,name='handlelogin'),
+    path('handleLogout/',etrack.handleLogout,name='handleLogout'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "home/reset_password.html"),name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="home/reset_password_sent.html"),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name ="home/password_reset_form.html"),name='password_reset_confirm'),
+    path('reset_password_complete/',auth_views.PasswordResetView.as_view(template_name ="home/password_reset_done.html"),name='password_reset_complete'),
+    path('addmoney/',etrack.addmoney,name='addmoney'),
+    path('addmoney_submission/',etrack.addmoney_submission,name='addmoney_submission'),
+    path('charts/',etrack.charts,name='charts'),
+    path('tables/',etrack.tables,name='tables'),
+    path('expense_edit/<int:id>',etrack.expense_edit,name='expense_edit'),
+    path('<int:id>/addmoney_update/', etrack.addmoney_update, name="addmoney_update") ,
+    path('expense_delete/<int:id>',etrack.expense_delete,name='expense_delete'),
+    path('profile/',etrack.profile,name = 'profile'),
+    path('expense_month/',etrack.expense_month, name = 'expense_month'),
+    path('stats/',etrack.stats, name = 'stats'),
+    path('expense_week/',etrack.expense_week, name = 'expense_week'),
+    path('weekly/',etrack.weekly, name = 'weekly'),
+    path('check/',etrack.check,name="check"),
+    path('search/',etrack.search,name="search"),
+    path('<int:id>/profile_edit/',etrack.profile_edit,name="profile_edit"),
+    path('<int:id>/profile_update/',etrack.profile_update,name="profile_update"),
+    path('info/',etrack.info,name="info"),
+    path('info_year/',etrack.info_year,name="info_year"),
+
+
+    # path('fityfeed/', efitty.home, name="fityfeed"),
 ]
